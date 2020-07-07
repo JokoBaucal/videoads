ccbf3636f6818c5706c70a468f08f6a6d10cc258
Joko Baucal <43583656+JokoBaucal@users.noreply.github.com>
2020-07-07 15:40:49 +0200

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,4 +1,8 @@
 var videoElement;
+var adsLoaded = false;
+var adContainer;
+var adDisplayContainer;
+var adsLoader;
 
 // On window load, attach an event to the play button click
 // that triggers playback on the video element
@@ -7,5 +11,30 @@ window.addEventListener('load', function(event) {
   var playButton = document.getElementById('play-button');
   playButton.addEventListener('click', function(event) {
     videoElement.play();
+    
+function initializeIMA() {
+  console.log("initializing IMA");
+  adContainer = document.getElementById('ad-container');
+  adDisplayContainer = new google.ima.AdDisplayContainer(adContainer, videoElement);
+  adsLoader = new google.ima.AdsLoader(adDisplayContainer);
+
+  // Let the AdsLoader know when the video has ended
+  videoElement.addEventListener('ended', function() {
+    adsLoader.contentComplete();
+  });
+
+  var adsRequest = new google.ima.AdsRequest();
+  adsRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?iu=/161634325/Blic_Video&description_url=https%3A%2F%2Fwww.blic.rs&tfcd=0&npa=0&sz=300x250%7C400x300%7C640x480&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=';
+
+  // Specify the linear and nonlinear slot sizes. This helps the SDK to
+  // select the correct creative if multiple are returned.
+  adsRequest.linearAdSlotWidth = videoElement.clientWidth;
+  adsRequest.linearAdSlotHeight = videoElement.clientHeight;
+  adsRequest.nonLinearAdSlotWidth = videoElement.clientWidth;
+  adsRequest.nonLinearAdSlotHeight = videoElement.clientHeight / 3;
+
+  // Pass the request to the adsLoader to request ads
+  adsLoader.requestAds(adsRequest);
+}
   });
 });
